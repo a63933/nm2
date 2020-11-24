49f6b74383fe700bda391fc2a9c76f418df12294
李政毅 <lizhengyi_m@163.com>
2020-11-24 20:38:30 +0800

    3

--- bin/nm2.js
+++ bin/nm2.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 
-process.argv.push('--cwd')
-process.argv.push(process.cwd())
 process.argv.push('--gulpfile')
 process.argv.push(require.resolve('../lib/index.js'))
+process.argv.push('--cwd')
+process.argv.push(process.cwd())
+
+console.log(process.argv)
+console.log("gulp is running")
 
 require('gulp/bin/gulp')
